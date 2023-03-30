c427557f531224c3000df40b24755e906f3316c8
PintoDrop <mikebender22@gmail.com>
2023-03-30 12:56:44 -0700

    adding to server.js

--- server.js
+++ server.js
@@ -2,3 +2,21 @@ const express = require("express");
 const routes = require("./routes");
 const sequelize = require("./config/connection");
 const dotenv = require("dotenv");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
+
+sequelize.sync({ force: false}).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+});
