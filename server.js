d41ffe8b3cf5a1f421ad9b92d9cffb7d0e2966a0
PintoDrop <mikebender22@gmail.com>
2023-03-31 08:54:25 -0700

    adding boiler plate code

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const dotenv = require("dotenv");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const { join } = require('./models');
+const passport = require('passport');
+const { User } = require('./models');
+const { Strategy: JWTStrategy, ExtractJwt } = require ('passport-jwt');
 
 
 const app = express();
@@ -14,7 +18,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(routes);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(User.createStrategy());
+passport.serializeUser((user, done) => {
+  dotenv(null, user.id)
+})
+
 
 
 sequelize.sync({ force: false}).then(() => {
